2caea43fda1688523a57af7b8843c62e3fe93031
JulianToledoGT <juliantoledogt@hotmail.com>
2020-02-10 09:51:26 -0600

    Ejercicio Finalizado
    Se ha englobado el código y se ha creado un método de inicialización dentro del mismo.

--- js/app.js
+++ js/app.js
@@ -7,13 +7,81 @@
 
 var teclas = document.getElementsByClassName("tecla");
 var mi_pantalla = document.getElementById("display");
-var en_curso = false;
-var resultado = 0;
-var operador = "";
-var valor = 0;
-var itr = 0;
 var x = 76.98; 
 var y = 62.91;
+
+var calculadora = {
+    en_curso: false,
+    itr: 0,
+    resultado: 0,
+    operador: "",
+    valor: 0,
+    inicializar: function(){
+        this.en_curso = false;
+        this.resultado = 0;
+        this.operador = "";
+        this.valor = 0;
+        this.itr = 0;
+    },
+    resultado8: function(mi_resultado){
+        var lng = 8;
+        var str_resultado = String(mi_resultado);
+        if (str_resultado.indexOf('.') != -1){lng += 1};
+        if (str_resultado.indexOf('-') != -1){lng += 1};
+        return str_resultado.substr(0,lng)
+    },
+    operar: function(signo) {
+        if (parseInt(signo)==signo){
+            if (mi_pantalla.innerText.replace("-","").replace(".","").length < 8){
+                if (mi_pantalla.innerText == "0"){
+                    mi_pantalla.innerText = signo;
+                } else {
+                    mi_pantalla.innerText += signo;
+                }
+            }
+        } else if (signo=="punto"){
+            if (mi_pantalla.innerText.indexOf('.') == -1){
+                mi_pantalla.innerText += ".";
+            }
+        } else if (signo=="sign"){
+            mi_pantalla.innerText *= -1;
+        } else if (signo=="raiz"){  //Se implemebtó, a pesar de no ser obligatorio
+            mi_pantalla.innerText = this.resultado8(Math.sqrt(parseFloat(mi_pantalla.innerText)));
+        } else if (signo=="on"){
+            this.inicializar;
+            mi_pantalla.innerText = "0";
+        } else if (signo=="igual"){
+            if (this.itr < 1){
+                this.valor = parseFloat(mi_pantalla.innerText)
+                this.itr += 1;
+            };
+            switch(this.operador){
+                case "mas":
+                    this.resultado += this.valor;
+                    break;
+                case "menos":
+                    this.resultado -= this.valor;
+                    break;
+                case "por":
+                    this.resultado *= this.valor;
+                    break;
+                case "dividido":
+                    this.resultado /= this.valor;
+                    break;
+            }
+            mi_pantalla.innerText = this.resultado8(this.resultado);
+        } else {
+            this.itr = 0;
+            this.operador = signo;
+            this.valor = parseFloat(mi_pantalla.innerText)
+            if (this.en_curso == false){
+                this.en_curso = true;
+                this.resultado = this.valor;
+            }
+            mi_pantalla.innerText = 0;
+        }
+    }
+}
 
 for (var i = 0; i < teclas.length; i++) {
     teclas[i].addEventListener("mousedown", function () {
@@ -27,8 +95,9 @@ for (var i = 0; i < teclas.length; i++) {
             tecla.style.width = xval + 'px';
             tecla.style.height = yval + 'px';
         };
-        operar(this.id);
+        calculadora.operar(this.id);
     });
+
 
     teclas[i].addEventListener("mouseup", function () {
         var tecla = document.getElementById(this.id);
@@ -41,66 +110,3 @@ for (var i = 0; i < teclas.length; i++) {
         }
     });
 };
-
-function operar(signo) {
-    if (parseInt(signo)==signo){
-        if (mi_pantalla.innerText.replace("-","").replace(".","").length < 8){
-            if (mi_pantalla.innerText == "0"){
-                mi_pantalla.innerText = signo;
-            } else {
-                mi_pantalla.innerText += signo;
-            }
-        }
-    } else if (signo=="punto"){
-        if (mi_pantalla.innerText.indexOf('.') == -1){
-            mi_pantalla.innerText += ".";
-        }
-    } else if (signo=="sign"){
-        mi_pantalla.innerText *= -1;
-    } else if (signo=="raiz"){
-        mi_pantalla.innerText = resultado8(Math.sqrt(parseFloat(mi_pantalla.innerText)));
-    } else if (signo=="on"){
-        en_curso = false;
-        resultado = 0;
-        operador = "";
-        valor = 0;
-        mi_pantalla.innerText = "0";
-    } else if (signo=="igual"){
-        if (itr < 1){
-            valor = parseFloat(mi_pantalla.innerText)
-            itr += 1;
-        };
-        switch(operador){
-            case "mas":
-                resultado += valor;
-                break;
-            case "menos":
-                resultado -= valor;
-                break;
-            case "por":
-                resultado *= valor;
-                break;
-            case "dividido":
-                resultado /= valor;
-                break;
-        }
-        mi_pantalla.innerText = resultado8(resultado);
-    } else {
-        itr = 0;
-        operador = signo;
-        valor = parseFloat(mi_pantalla.innerText)
-        if (en_curso == false){
-            en_curso = true;
-            resultado = valor;
-        }
-        mi_pantalla.innerText = 0;
-    }
-}
-
-function resultado8 (mi_resultado){
-    var lng = 8;
-    var str_resultado = String(mi_resultado);
-    if (str_resultado.indexOf('.') != -1){lng += 1};
-    if (str_resultado.indexOf('-') != -1){lng += 1};
-    return str_resultado.substr(0,lng)
-}
